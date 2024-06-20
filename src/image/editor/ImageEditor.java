4841e38c4089b1976438985c04fb01d034777139
LeulMessle <162681839+LeulMessle@users.noreply.github.com>
2024-06-20 12:09:15 +0200

    Update ImageEditor.java
    Added trim feature and negative color feature

--- src/image/editor/ImageEditor.java
+++ src/image/editor/ImageEditor.java
@@ -71,4 +71,50 @@ public class ImageEditor {
 
     }
 
-}
+ public static int[][] trimBorders(int[][] imageTwoD, int pixelCount){
+        
+        if(imageTwoD.length > pixelCount * 2 && imageTwoD[0].length > pixelCount * 2){
+            
+            //We multiply the pixel count by 2 because we are trimming from both sides
+            int[][] trimmedImage = new int[imageTwoD.length - pixelCount * 2]
+                    [imageTwoD[0].length - pixelCount * 2];
+            
+            for(int i=0; i<trimmedImage.length; i++){
+                for(int j=0; j<trimmedImage[i].length; j++){
+                    
+                    //We are adding the pixelCount here because we want to jump
+                    //pixels before that
+                    trimmedImage[i][j] = imageTwoD[i + pixelCount][j + pixelCount];
+                }
+            }
+            
+            return trimmedImage;
+        }
+        
+        else{
+            
+            System.out.println("Cannot trim that many pixels from given image.");
+            return imageTwoD;
+        }
+    }
+    
+    public static int[][] negativeColor(int[][] imageTwoD){
+        
+        int[][] negativeImage = new int[imageTwoD.length][imageTwoD[0].length];
+        
+        for(int i=0; i<negativeImage.length; i++){
+            for(int j=0; j<negativeImage[0].length; j++){
+                
+                int[] rgba = getRGBAFromPixel(imageTwoD[i][j]);
+                rgba[0] = 255 - rgba[0];
+                rgba[1] = 255 - rgba[1];
+                rgba[2] = 255 - rgba[2];
+                
+                negativeImage[i][j] = getColorIntValueFromRGBA(rgba);
+            }
+        }
+        
+        return negativeImage;
+    }
+    
+   
