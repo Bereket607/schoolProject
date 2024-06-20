9fac06ab6078ded52a86cdc37ea704645b8a2bb7
LeulMessle <162681839+LeulMessle@users.noreply.github.com>
2024-06-20 13:52:55 +0200

    Update ImageEditor.java

--- src/image/editor/ImageEditor.java
+++ src/image/editor/ImageEditor.java
@@ -18,57 +18,191 @@ import javax.imageio.ImageIO;
  */
 public class ImageEditor {
 
+  
     public static void main(String[] args) {
-
+        
         System.out.println("Enter in the location of the image");
         Scanner stringScan = new Scanner(System.in);
         String locInput = stringScan.next();
-
+        
         System.out.println("****************************************************");
-
+        
         System.out.println("What Do you want to do? ");
         System.out.print("1. Trim the borders of the image        ");
         System.out.println("2. Show negative color of Image");
         System.out.print("3. Stretch Horizontally                 ");
         System.out.println("4. Shrink Vertically");
         System.out.println("5. Invert the image");
-
+        
         System.out.println("****************************************************");
-
+        
         Scanner scan = new Scanner(System.in);
         int input = scan.nextInt();
-
+        
         int[][] imageData = MainProcesses.imageToTwoD(locInput);
         String finalLocation = pathModifier(locInput);
-
-        switch (input) {
+        
+        switch(input){
             case 1:
                 int[][] trimmed = trimBorders(imageData, 60);
                 MainProcesses.twoDToImage(trimmed, finalLocation.concat("trimmed.jpg"));
                 break;
-
+             
             case 2:
                 int[][] negative = negativeColor(imageData);
                 MainProcesses.twoDToImage(negative, finalLocation.concat("negative.jpg"));
                 break;
-
+                
             case 3:
                 int[][] stretchImageHorizontally = stretchHorizontally(imageData);
                 MainProcesses.twoDToImage(stretchImageHorizontally, finalLocation.concat("stretched.jpg"));
                 break;
-
+                
             case 4:
                 int[][] shrinkImageVertically = shrinkVertically(imageData);
                 MainProcesses.twoDToImage(shrinkImageVertically, finalLocation.concat("shrunk.jpg"));
                 break;
-
+        
             case 5:
                 int[][] invertedImage = invertImage(imageData);
                 MainProcesses.twoDToImage(invertedImage, finalLocation.concat("inverted.jpg"));
                 break;
-
-        }
-
+               
+              }
+        
+        
+        
+        
     }
-
+    
+    public static int[][] trimBorders(int[][] imageTwoD, int pixelCount){
+        
+        if(imageTwoD.length > pixelCount * 2 && imageTwoD[0].length > pixelCount * 2){
+            
+            //We multiply the pixel count by 2 because we are trimming from both sides
+            int[][] trimmedImage = new int[imageTwoD.length - pixelCount * 2]
+                    [imageTwoD[0].length - pixelCount * 2];
+            
+            for(int i=0; i<trimmedImage.length; i++){
+                for(int j=0; j<trimmedImage[i].length; j++){
+                    
+                    //We are adding the pixelCount here because we want to jump
+                    //pixels before that
+                    trimmedImage[i][j] = imageTwoD[i + pixelCount][j + pixelCount];
+                }
+            }
+            
+            return trimmedImage;
+        }
+        
+        else{
+            
+            System.out.println("Cannot trim that many pixels from given image.");
+            return imageTwoD;
+        }
+    }
+    
+    public static int[][] negativeColor(int[][] imageTwoD){
+        
+        int[][] negativeImage = new int[imageTwoD.length][imageTwoD[0].length];
+        
+        for(int i=0; i<negativeImage.length; i++){
+            for(int j=0; j<negativeImage[0].length; j++){
+                
+                int[] rgba = getRGBAFromPixel(imageTwoD[i][j]);
+                rgba[0] = 255 - rgba[0];
+                rgba[1] = 255 - rgba[1];
+                rgba[2] = 255 - rgba[2];
+                
+                negativeImage[i][j] = getColorIntValueFromRGBA(rgba);
+            }
+        }
+        
+        return negativeImage;
+    }
+    
+    public static int[][] stretchHorizontally(int[][] imageTwoD){
+        
+        int[][] stretchedImage = new int[imageTwoD.length][2*imageTwoD[0].length];
+        int pos = 0;
+        
+        for(int i=0; i<imageTwoD.length; i++){
+            for(int j=0; j<imageTwoD[0].length; j++){
+                
+                pos = j * 2;
+                stretchedImage[i][pos] = imageTwoD[i][j];
+                stretchedImage[i][pos + 1] = imageTwoD[i][j];
+            }
+        }
+        
+        return stretchedImage;
+    }
+    
+    public static int[][] shrinkVertically(int[][] imageTwoD){
+        
+        int[][] shrunkImage = new int[imageTwoD.length / 2][imageTwoD[0].length];
+        
+        for(int i=0; i<imageTwoD[0].length; i++){
+            for(int j=0; j<imageTwoD.length - 1; j+=2){
+                
+                shrunkImage[j/2][i] = imageTwoD[j][i];
+            }
+        }
+        
+        return shrunkImage;
+    }
+    
+    public static int[][] invertImage(int[][] imageTwoD){
+        
+        int[][] invertedImage = new int[imageTwoD.length][imageTwoD[0].length];
+        
+        for(int i=0; i<imageTwoD.length; i++){
+            for(int j=0; j<imageTwoD[0].length; j++){
+                
+                invertedImage[i][j] = imageTwoD[(imageTwoD.length-1) - i][(imageTwoD[0].length-1) - j];
+            }
+        }
+        
+        return invertedImage;
+    }
+    
+    public static String pathModifier(String input){
+        
+        char[] inputChar = input.toCharArray();
+                StringBuilder sb = new StringBuilder();
+                sb.append(input);
+                for(int i=inputChar.length-1; i>=0; i--){
+                    if(inputChar[i] == ('\\')){
+                        break;
+                    }
+                    sb.deleteCharAt(i);
+                }
+                inputChar = sb.toString().toCharArray();
+                String finalLocation = String.valueOf(inputChar);
+                return finalLocation;
+    }
+    
+    public static int[] getRGBAFromPixel(int pixelColorValue){
+        
+        Color pixelColor = new Color(pixelColorValue);
+        
+        return new int[]{ pixelColor.getRed(), pixelColor.getGreen()
+                , pixelColor.getBlue(), pixelColor.getAlpha()};
+    }
+    
+    public static int getColorIntValueFromRGBA(int[] colorData){
+        
+        if(colorData.length == 4){
+            
+            Color color = new Color(colorData[0], colorData[1], 
+                    colorData[2], colorData[3]);
+            return color.getRGB();
+        }
+        else{
+            
+            System.out.println("Incorrect number of elements in RGBA array");
+            return -1;
+        }
+    }
+    
 }
